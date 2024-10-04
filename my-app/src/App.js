10aa07df571d0ff74d015a6f51fc9660646d93be
Sophie Stone <smstone04@gmail.com>
2024-10-04 14:20:53 +0100

    Set metadata

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function HelloWorld() {
-  return <h1 className="greeting">Hello, world!</h1>;
+function Intro() {
+  return <h1 className="greeting">Sophie Stone</h1>;
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld></HelloWorld>
+        <Intro></Intro>
       </header>
     </div>
   );
