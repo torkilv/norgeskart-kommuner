50f717f573aa40ece6c4ad62a97bb070c683fbc2
Sophie Stone <smstone04@gmail.com>
2024-10-06 23:43:50 +0100

    Zoom on img hover

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,11 @@ function Header() {
     <h1>Sophie Stone</h1>
     <div id="icons">
     <a href="https://github.com/smstone0">    
-      <img src="github-mark/github-mark-white.svg" alt="GitHub" width="32.66" height= "32"></img>
+    <img src="assets/github-mark/github-mark-white.svg" alt="GitHub" width="32.66" height= "32"></img>
     </a>
     <div id="icon-padding"></div>
     <a href="https://www.linkedin.com/in/sophie-stone-/">    
-      <img src="/In-White-96.png" alt="LinkedIn" width="32" height= "32"></img>
+    <img src="/assets/In-White-96.png" alt="LinkedIn" width="32" height= "32"></img>
     </a>
     </div>
   </div>
@@ -40,11 +40,11 @@ function Project({project}) {
     <div class="project">
       <p id="date">{project.date}</p>
       <h3>{project.title}</h3>
-      <a href={project.url}>
         <div className="image-container">
-          <img src={project.thumbnail} alt={project.url}></img>
+          <a className="image-container" href={project.url}>
+            <img src={project.thumbnail} alt={project.url}></img>
+          </a>
         </div>
-      </a>
       <p>{project.description}</p>
     </div>
   );  
--- my-app/src/projects.js
+++ my-app/src/projects.js
@@ -3,17 +3,17 @@ const projects = [
         id: 1,
         title: "Software Engineering Group Project",
         date: "Sep 2023 - Jun 2024",
-        description: "University group project",
+        description: "University group project to develop an event-finding app for industry partner, Lots For Tots",
         url: "https://www.figma.com/design/pBlTcItDic1lt4CA2VA81k/Prototyping?t=Jc90sSWYw35fi01r-0",
-        thumbnail: "lots-for-tots.png"
+        thumbnail: "assets/lft/lots-for-tots.png"
     },
     {
         id: 2,
         title: "Caloric",
         date: "Jun 2023 - Sep 2023",
-        description: "Calorie counting app",
+        description: "Calorie counting app with minimalist features",
         url: "https://www.figma.com/design/ojnG7Z2LYuzCuDM0TMUUiA/Caloric?node-id=0-1&node-type=canvas&t=lJdGKqTurMDbI7DR-0",
-        thumbnail: "caloric.png"
+        thumbnail: "assets/caloric/caloric.png"
     },
 ]
 
