06eed5799b81256eb081d758cbf1830e8447e6ac
Sophie Stone <smstone04@gmail.com>
2024-12-22 22:56:07 +0000

    Add languages property to project

--- my-app/src/projects.js
+++ my-app/src/projects.js
@@ -5,7 +5,8 @@ const projects = [
         date: "Sep 2023 - Jun 2024",
         description: "University group project to develop an event-finding app for industry partner, Lots For Tots",
         url: "https://www.figma.com/design/pBlTcItDic1lt4CA2VA81k/Prototyping?t=Jc90sSWYw35fi01r-0",
-        thumbnail: "assets/lft/lots-for-tots.png"
+        thumbnail: "assets/lft/lots-for-tots.png",
+        languages: ["Flutter", "Figma"]
     },
     {
         id: 2,
@@ -13,7 +14,8 @@ const projects = [
         date: "Jun 2023 - Sep 2023",
         description: "Calorie counting app with minimalist features",
         url: "https://www.figma.com/design/ojnG7Z2LYuzCuDM0TMUUiA/Caloric?node-id=0-1&node-type=canvas&t=lJdGKqTurMDbI7DR-0",
-        thumbnail: "assets/caloric/caloric.png"
+        thumbnail: "assets/caloric/caloric.png",
+        languages: ["Flutter", "Figma", "SQL"]
     },
 ]
 
