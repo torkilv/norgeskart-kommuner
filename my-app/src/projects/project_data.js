cdb274916807abff0cac95955195c7e131867d28
Sophie Stone <smstone04@gmail.com>
2024-12-28 23:21:11 +0000

    Add multi-line support for description

--- my-app/src/projects/project_data.js
+++ my-app/src/projects/project_data.js
@@ -2,7 +2,7 @@ const projects = [
     {
         title: "Caloric",
         date: "Jun 2023 - present",
-        description: "Calorie counting app with minimalist features",
+        description: `Calorie counting app with minimalist features`,
         url: "https://www.figma.com/design/ojnG7Z2LYuzCuDM0TMUUiA/Caloric?node-id=0-1&node-type=canvas&t=lJdGKqTurMDbI7DR-0",
         thumbnail: "assets/caloric/caloric.png",
         languages: ["Flutter", "SQL", "Figma"]
@@ -10,7 +10,7 @@ const projects = [
     {
         title: "Software Engineering Group Project",
         date: "Sep 2023 - Jun 2024",
-        description: "University group project to develop an event-finding app for industry partner, Lots For Tots",
+        description: `University group project to develop an event-finding app for industry partner, Lots For Tots`,
         url: "https://www.figma.com/design/pBlTcItDic1lt4CA2VA81k/Prototyping?t=Jc90sSWYw35fi01r-0",
         thumbnail: "assets/lots-for-tots/lots-for-tots.png",
         languages: ["Flutter", "Figma"]
@@ -18,7 +18,7 @@ const projects = [
     {
         title: "Operating Systems and Concurrency",
         date: "Oct 2023 - Jan 2024",
-        description: "University project to implement a process and I/O management simulation via the POSIX API in Linux",
+        description: `University project to implement a process and I/O management simulation via the POSIX API in Linux`,
         thumbnail: "assets/nottingham.svg",
         languages: ["C"]
     },
