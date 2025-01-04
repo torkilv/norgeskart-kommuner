18f89c1b341e94175c770308f71bdaa6f5865c8a
Sophie Stone <smstone04@gmail.com>
2025-01-04 23:37:12 +0000

    Make name clickable

--- my-app/src/header/Header.js
+++ my-app/src/header/Header.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import {ReactComponent as GithubIcon} from '../assets/github-mark-white.svg';
 import {ReactComponent as LinkedInIcon} from '../assets/linkedin.svg';
 import './Header.css';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return <div id="Header">
-      <h1 id="name">Sophie Stone</h1>
+      <Link id="name-link" to="/">
+        <h1 id="name">Sophie Stone</h1>
+      </Link>
       <div id="icons">
         <a href="https://github.com/smstone0"><GithubIcon/></a>
         <a href="https://www.linkedin.com/in/sophie-stone-/"><LinkedInIcon/></a>
