67a845c729fe0fd51be28a0bebda7426a65cfcf9
Sophie Stone <smstone04@gmail.com>
2025-01-04 15:57:57 +0000

    Test BrowserRouter for cleaner URLs with 404 redirect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,17 @@
-import React from 'react';
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
 import MapUK from './pages/MapUK.js';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/uk-map" element={<MapUK/>}/>
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
 export default App;
-
-// import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './pages/Home.js';
-// import MapUK from './pages/MapUK.js';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home/>}/>
-//         <Route path="/uk-map" element={<MapUK/>} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
