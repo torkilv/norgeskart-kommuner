781ca0377a6b8a7a21f147f7ef70c03e68c62858
Sophie Stone <smstone04@gmail.com>
2025-01-04 15:34:40 +0000

    Test router change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,36 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
 import MapUK from './pages/MapUK.js';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/uk-map" element={<MapUK/>} />
+        <Route path="/uk-map" element={<MapUK/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
 export default App;
+
+// import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home.js';
+// import MapUK from './pages/MapUK.js';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/uk-map" element={<MapUK/>} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
