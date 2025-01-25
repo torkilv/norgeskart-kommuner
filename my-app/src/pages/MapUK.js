3b73b04f9532dcb3183b85238f27ab74330835f8
Sophie Stone <smstone04@gmail.com>
2025-01-25 14:28:58 +0000

    Use states for selected county and card position

--- my-app/src/pages/MapUK.js
+++ my-app/src/pages/MapUK.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as Map} from '../assets/MapChart_Map.svg';
 
 function MapUK() {
+    const [selectedCounty, setSelectedCounty] = useState(null);
+    const [cardPosition, setCardPosition] = useState({x: 0, y: 0});
+
+    const countyClick = (e) => {
+        const target = e.target;
+        if(target.tagName === "path" && target.hasAttribute("name")) {
+            const countyName = target.getAttribute("name");
+            const {clientX: x, clientY: y} = e;
+            setSelectedCounty(countyName);
+            setCardPosition({x, y});
+        } else {
+            setSelectedCounty(null);
+        }
+    }
+
     return (
         <div id="MapUK">
+            {selectedCounty && <CountyCard countyName={selectedCounty} position={cardPosition}/>}
             <div id="map-container">
-                <Map/>
+                <Map id="map" onClick={countyClick}/>
             </div>
+        </div>
+    )
+}
+
+function CountyCard({countyName, position}) {
+    const {x, y} = position;
+    return (
+        <div id="county-card" onClick={(e) => e.stopPropagation()} style={{
+            left: `${x}px`,
+            top: `${y}px`,
+        }}>
+            <h2>{countyName}</h2>
         </div>
     )
 }
