29da665235cc3fc0da95d984dd6a61a00e5e61aa
Sophie Stone <smstone04@gmail.com>
2025-01-26 15:38:32 +0000

    Update county colour and total level upon level click

--- my-app/src/pages/MapUK.js
+++ my-app/src/pages/MapUK.js
@@ -3,7 +3,20 @@ import {ReactComponent as Map} from '../assets/MapChart_Map.svg';
 
 function MapUK() {
     const [selectedCounty, setSelectedCounty] = useState(null);
+    const [selectedCountyElement, setSelectedCountyElement] = useState(null);
     const [cardPosition, setCardPosition] = useState({x: 0, y: 0});
+    const [countyScores, setCountyScores] = useState({});
+
+    const getColorByLevel = (level) => {
+        switch(level) {
+            case 5: return '#FF7E7E'; // lived
+            case 4: return '#FFB57E'; // stayed
+            case 3: return '#FFE57E'; // visited
+            case 2: return '#A8FFBE'; // stopped
+            case 1: return '#88AEFF'; // passed
+            default: return 'white'; // never been
+        }
+    };
 
     const countyClick = (e) => {
         const target = e.target;
@@ -11,15 +24,29 @@ function MapUK() {
             const countyName = target.getAttribute("name");
             const {clientX: x, clientY: y} = e;
             setSelectedCounty(countyName);
+            setSelectedCountyElement(target);
             setCardPosition({x, y});
         } else {
             setSelectedCounty(null);
+            setSelectedCountyElement(null);
         }
+    }
+
+    const levelClick = (level) => {
+        selectedCountyElement.style.fill = getColorByLevel(level);
+
+        const updatedScores = {...countyScores, [selectedCounty]: level};
+        setCountyScores(updatedScores);
+        const totalLevel = Object.values(updatedScores).reduce((acc, curr) => acc + curr, 0);
+        const levelElement = document.getElementById('level');
+        levelElement.textContent = `UK Level ${totalLevel}`;
+        
+        setSelectedCounty(null); //close card upon selection
     }
 
     return (
         <div id="MapUK">
-            {selectedCounty && <CountyCard countyName={selectedCounty} position={cardPosition}/>}
+            {selectedCounty && <CountyCard countyName={selectedCounty} position={cardPosition} levelClick={levelClick}/>}
             <div id="map-container">
                 <Map id="map" onClick={countyClick}/>
             </div>
@@ -27,7 +54,7 @@ function MapUK() {
     )
 }
 
-function CountyCard({countyName, position}) {
+function CountyCard({countyName, position, levelClick}) {
     const {x, y} = position;
     return (
         <div id="county-card" onClick={(e) => e.stopPropagation()} style={{
@@ -36,12 +63,12 @@ function CountyCard({countyName, position}) {
         }}>
             <h3 id="county-name">{countyName}</h3>
             <div id='card-options'>
-                <p id='lived'>Lived here</p>
-                <p id='stayed'>Stayed here</p>
-                <p id='visited'>Visited here</p>
-                <p id='stopped'>Stopped here</p>
-                <p id='passed'>Passed here</p>
-                <p id='never-been'>Never been here</p>
+                <p id='lived' onClick={() => levelClick(5)}>Lived here</p>
+                <p id='stayed' onClick={() => levelClick(4)}>Stayed here</p>
+                <p id='visited' onClick={() => levelClick(3)}>Visited here</p>
+                <p id='stopped' onClick={() => levelClick(2)}>Stopped here</p>
+                <p id='passed' onClick={() => levelClick(1)}>Passed here</p>
+                <p id='never-been' onClick={() => levelClick(0)}>Never been here</p>
             </div>
         </div>
     )
