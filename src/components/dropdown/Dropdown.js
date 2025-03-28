14e06753d8c27ae8ed3072644fad18ed82087ff0
Sophie Stone <smstone04@gmail.com>
2025-03-28 19:38:24 +0000

    Close dropdown on click outside

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,17 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './Dropdown.css';
 import {ReactComponent as Chevron} from '../../assets/down-arrow.svg';
 
 function Dropdown({ allTechStacks, selectedTechStacks, onTechStackChange }) {
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+  
+    useEffect(() => {
+      if (dropdownOpen) {
+        document.addEventListener('mousedown', handleClickOutside);
+      } else {
+        document.removeEventListener('mousedown', handleClickOutside);
+      }
+  
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [dropdownOpen]);
+
     return (
-    <div className="dropdown">
-      <button className="dropbtn" onClick={toggleDropdown}>Filter Tech Stack <Chevron/> </button>
+    <div className="dropdown" ref={dropdownRef}>
+      <button className="dropbtn" onClick={toggleDropdown}>
+        Filter Tech Stack
+        <Chevron/>
+      </button>
       {dropdownOpen && <div className="dropdown-content">
         {allTechStacks.map((techStack) => (
           <label key={techStack} className="dropdown-item">
