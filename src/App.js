945ac3dfbf29df065667bdd3cae3c1d40ef1b571
Torkil Vederhus <torkilv@gmail.com>
2025-05-31 09:15:32 +0200

    Deleted unused files and added a proper preview

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NorwegianMap from "./components/NorwegianMap.js";
-import { LanguageProvider } from './context/LanguageContext';
 
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<NorwegianMap  />} />
-        </Routes>
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NorwegianMap />} />
+      </Routes>
+    </Router>
   );
 }
 
