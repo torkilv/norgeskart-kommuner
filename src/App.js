c7784039db260e2ceefc0ecd579902a00f5ba507
Torkil Vederhus <torkilv@gmail.com>
2025-05-31 19:01:59 +0200

    Cleaned up repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NorwegianMap from "./components/NorwegianMap.js";
+import AnimalConflicts from "./components/AnimalConflicts.js";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<NorwegianMap />} />
+        <Route path="/dyrekonflikter" element={<AnimalConflicts />} />
       </Routes>
     </Router>
   );
